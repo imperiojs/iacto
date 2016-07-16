5de8b77dc8e022da48772ee8df8ed60a63641d82
Matthew McLaughlin <Matthew McLaughlin>
2016-07-15 19:42:11 -0700

    gesture example for presentation and website functional

--- client/mobile.js
+++ client/mobile.js
@@ -1,5 +1,6 @@
 var swipeDiv = document.getElementById('swipe');
 var pinchDiv = document.getElementById('pinch-box');
+var panDiv = document.getElementById('pan-box');
 
 imperio.mobileRoomSetup(imperio.socket, imperio.room);
 
@@ -11,5 +12,30 @@ function handlePinch (event) {
   console.log('detecting pinch');
 }
 
+function handlePan (event) {
+  console.log('detecting pan');
+}
+
+function handleRotate (event) {
+  console.log('detecting rotation');
+}
+
+function handlePress (event) {
+  console.log('detecting press');
+}
+
 imperio.curse('swipe', swipeDiv, swipeCount);
 imperio.curse('pinch', pinchDiv, handlePinch);
+imperio.curse('pinchStart', pinchDiv, handlePinch);
+imperio.curse('pinchEnd', pinchDiv, handlePinch);
+
+imperio.curse('pan', panDiv, handlePan);
+imperio.curse('panStart', panDiv, handlePan);
+imperio.curse('panEnd', panDiv, handlePan);
+
+imperio.curse('rotate', panDiv, handleRotate);
+imperio.curse('rotateStart', panDiv, handleRotate);
+imperio.curse('rotateEnd', panDiv, handleRotate);
+
+imperio.curse('press', panDiv, handlePress);
+imperio.curse('pressUp', panDiv, handlePress);
